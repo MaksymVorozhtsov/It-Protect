959e71af537d611adf766e6c4eaad6c5aa3115ce
Maksym Vorozhtsov <31190340+MaksymVorozhtsov@users.noreply.github.com>
2017-12-09 23:45:27 +0100

    v1.3
    popup

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+function OffScroll () {
+    var winScrollTop = $(window).scrollTop();
+        $(window).bind('scroll',function () {
+        $(window).scrollTop(winScrollTop);
+    });
+}
+
+$(document).ready(function () {
+	$('a#cost').click(function (event){
+		event.preventDefault();
+		$('#overlay').fadeIn(400, function (){
+            $('#modal').css('display', 'block').animate({opacity: 1}, 200);
+            OffScroll();
+		});
+	});
+	$('#overlay, .modal-close').click( function (){
+		$('#modal').animate({opacity: 0}, 200,
+            function(){
+                $(this).css('display', 'none');
+                $('#overlay').fadeOut(400);
+                $(window).unbind('scroll');
+            }
+        );
+	});
+});
+
 new WOW().init();
